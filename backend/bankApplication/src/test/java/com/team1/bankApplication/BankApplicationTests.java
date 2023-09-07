ea2b345eb0a40209df192113f158066499cea927
Nirvik Agarwal <nirvik.agarwal@gmail.com>
2023-09-07 23:23:19 +0530

    [+] spring initial setup

---
+++ backend/bankApplication/src/test/java/com/team1/bankApplication/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.bankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
