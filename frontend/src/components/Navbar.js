19eb140ec900d84a42a1186e97b4fb31ce8aba00
Aman <kaman120702@gmail.com>
2023-09-18 23:38:13 +0530

    css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
               Login
             </NavLink>
           </li>
-          {/* <li className="nav-item">
+          <li className="nav-item">
             <NavLink
               style={navLinkStyle}
               className="nav-link hov"
@@ -58,7 +58,7 @@ const Navbar = () => {
             >
               user table
             </NavLink>
-          </li> */}
+          </li>
           <li className="nav-item">
             <NavLink
               style={navLinkStyle}
@@ -67,6 +67,16 @@ const Navbar = () => {
               activeClassName="active"
             >
               Open Account
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              style={navLinkStyle}
+              className="nav-link hov"
+              to="/netBanking"
+              activeClassName="active"
+            >
+              Net Banking
             </NavLink>
           </li>
         </ul>
@@ -87,106 +97,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// import { Link } from "react-router-dom";
-
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <div className="container-fluid">
-//         <Link to="/" className="navbar-brand">
-//           Bank Name
-//         </Link>
-//         <button
-//           className="navbar-toggler"
-//           type="button"
-//           data-bs-toggle="collapse"
-//           data-bs-target="#navbarSupportedContent"
-//           aria-controls="navbarSupportedContent"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span className="navbar-toggler-icon" />
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//             <li className="nav-item">
-//               <Link className="nav-link active" aria-current="page" to="/">
-//                 Home
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link className="nav-link active" to="/userRegistration">
-//                 Register
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link className="nav-link active" to="/login">
-//                 Login
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link className="nav-link active" to="/userTable">
-//                 user table
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link className="nav-link active" to="/createAccount">
-//                 Open Account
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link className="nav-link active" to="/netBanking">
-//                 Net Banking
-//               </Link>
-//             </li>
-//             {/* <li className="nav-item dropdown">
-//               <div
-//                 className="nav-link dropdown-toggle"
-//                 id="navbarDropdown"
-//                 role="button"
-//                 data-bs-toggle="dropdown"
-//                 aria-expanded="false"
-//               >
-//                 Dropdown
-//               </div>
-//               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <li>
-//                   <a className="dropdown-item" href="#">
-//                     Action
-//                   </a>
-//                 </li>
-//                 <li>
-//                   <a className="dropdown-item" href="#">
-//                     Another action
-//                   </a>
-//                 </li>
-//                 <li>
-//                   <hr className="dropdown-divider" />
-//                 </li>
-//                 <li>
-//                   <a className="dropdown-item" href="#">
-//                     Something else here
-//                   </a>
-//                 </li>
-//               </ul>
-//             </li> */}
-//           </ul>
-//           <form className="d-flex">
-//             <input
-//               className="form-control me-2"
-//               type="search"
-//               placeholder="Search"
-//               aria-label="Search"
-//             />
-//             <button className="btn btn-outline-success" type="submit">
-//               Search
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
