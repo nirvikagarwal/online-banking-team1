3c7bc7399554c728cea1cc048dfd86cb36b07c90
Aditya Khanna <khannaaditya9106@gmail.com>
2023-09-19 23:03:25 +0530

    net banking

--- frontend/src/pages/NetBankingPage.js
+++ frontend/src/pages/NetBankingPage.js
@@ -1,29 +1,63 @@
 import { Link } from "react-router-dom";
+import "./netBankingPage.css";
 
 const NetBankingPage = () => {
   return (
-    <div class="container mt-5" style={{ height: "500px" }}>
-      <div class="row">
-        <div class="col-md-3">
-          <Link to="/fundTransfer">
-            <button class="btn btn-light btn-block">Fund Transfer</button>
-          </Link>
-        </div>
-        <div class="col-md-3">
-          <Link to="/addBeneficiary">
-            <button class="btn btn-light btn-block">Add Beneficiary</button>
-          </Link>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6 col-lg-6 mt-5">
+          {/* Fund Transfer Service */}
+          <div className="card mb-4 card1">
+            <div className="card-body">
+              <h5 className="card-title">Fund Transfer</h5>
+              <p className="card-text">Transfer money to another account.</p>
+              <a href="#" className="btn btn-outline-light">
+                Go to Fund Transfer
+              </a>
+            </div>
+          </div>
         </div>
 
-        <div class="col-md-3">
-          <Link to="/manageBeneficiary">
-            <button class="btn btn-light btn-block">Manage Beneficiary</button>
-          </Link>
+        <div className="col-md-6 col-lg-6 mt-5">
+          {/* Add Beneficiary Service */}
+          <div className="card mb-4 card1">
+            <div className="card-body">
+              <h5 className="card-title">Add Beneficiary</h5>
+              <p className="card-text">
+                Add a new beneficiary for easy transfers.
+              </p>
+              <a href="#" className="btn btn-outline-light">
+                Go to Add Beneficiary
+              </a>
+            </div>
+          </div>
         </div>
-        <div class="col-md-3">
-          <Link to="/transactions">
-            <button class="btn btn-light btn-block">Transaction History</button>
-          </Link>
+
+        <div className="col-md-6 col-lg-6 mt-4 mb-5">
+          {/* Manage Beneficiary Service */}
+          <div className="card mb-4 card1">
+            <div className="card-body">
+              <h5 className="card-title">Manage Beneficiary</h5>
+              <p className="card-text">View and manage your beneficiaries.</p>
+              <a href="#" className="btn btn-outline-light">
+                Go to Manage Beneficiary
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Add more services as needed */}
+        <div className="col-md-6 col-lg-6 mt-4 mb-5">
+          {/* Manage Beneficiary Service */}
+          <div className="card mb-4 card1">
+            <div className="card-body">
+              <h5 className="card-title">Manage Beneficiary</h5>
+              <p className="card-text">View and manage your beneficiaries.</p>
+              <a href="#" className="btn btn-outline-light">
+                Go to Manage Beneficiary
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
